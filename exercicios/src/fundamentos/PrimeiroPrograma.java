147a676d07563efa5a2c762969d55f4d822a6af1
Bruno A. Santana <brunosantana87@gmail.com>
2022-08-09 12:14:36 -0300

    Initial commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,42 @@
+package fundamentos;
+
+/**
+ * Essa classe representa... JAVADOC
+ * 
+ * @author Bruno Aarão Santana <brunosantana87@gmail.com> 
+ * @email brunosantana87@gmail.com
+ * @since 1.0
+ *
+ */
+
+public class PrimeiroPrograma {
+	
+	/**
+	 * 
+	 * @param args
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		// Comentário de uma linha
+		// Uma sentença de código termina com ;
+		System.out.println("Olá, mundo! Primeiro programa! Botafogo será campeão!");
+		System.out.println("Primeiro programa Parte #01!");
+		System.out.println("Primeiro programa Parte #02!");
+		
+		/*
+		 *  Comentário de múltiplas linhas
+		 *  Linha 1
+		 *  Linha 2
+		 */
+		
+//		System.out.println();
+		// Transformar uma linha em comentário: "ctrl + /" em qualquer lugar da linha
+		
+		/**
+		 * Isso é comentário javadoc
+		 * Com o javadoc vc pode colocar informações que serão lidas p/ gerar a documentação 
+		 */
+	}
+}
